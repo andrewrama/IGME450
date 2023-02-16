e61abfcfbff8b3e170f1fc9469b7bf2941ded799
Jabrecia Washington <jlw4958@g.rit.edu>
2023-02-15 22:02:00 -0500

    cleaned up player script

--- Assets/Scripts/EndlessRunnerScripts/playerScript.cs
+++ Assets/Scripts/EndlessRunnerScripts/playerScript.cs
@@ -6,15 +6,11 @@ public class playerScript : MonoBehaviour
 
 {
     public int velocity;
-
     public Vector2 screenBounds;
-
-    //public Camera gameCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        //gameCamera = FindObjectOfType<Camera>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); //gives us half the screen width and half the screen height (but they're negative values!)
     }
 
@@ -23,15 +19,11 @@ public class playerScript : MonoBehaviour
     {
         // check if player has clicked 'A' or 'D'
         // move left or right
-        // + gameScreen.GetComponent<RectTransform>().rect.width
-        // + gameScreen.GetComponent<RectTransform>().rect.width
-        // transform.position.x >= (gameCamera.transform.position.x - (gameCamera.fieldOfView/2))
 
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.position -= new Vector3(velocity, 0, 0);
-            //transform.position -= new Vector3(Mathf.Clamp(transform.position.x, gameScreen.GetComponent<RectTransform>().rect.x, gameScreen.GetComponent<RectTransform>().rect.width), 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
