f4cec9e034d9692e5ed7f4de867061725e883e13
Jabrecia Washington <jlw4958@g.rit.edu>
2023-02-15 21:33:33 -0500

    working on keeping player within screen bounds

--- Assets/Scripts/EndlessRunnerScripts/playerScript.cs
+++ Assets/Scripts/EndlessRunnerScripts/playerScript.cs
@@ -6,16 +6,16 @@ public class playerScript : MonoBehaviour
 
 {
     public int velocity;
-    public Canvas gameScreen;
-    float screenHeight;
-    float screenWidth;
+
+    public Vector2 screenBounds;
+
+    //public Camera gameCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameScreen = FindObjectOfType<Canvas>();
-        screenWidth = gameScreen.GetComponent<RectTransform>().rect.width;
-        screenHeight = gameScreen.GetComponent<RectTransform>().rect.height;
+        //gameCamera = FindObjectOfType<Camera>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); //gives us half the screen width and half the screen height (but they're negative values!)
     }
 
     // Update is called once per frame
@@ -23,15 +23,28 @@ public class playerScript : MonoBehaviour
     {
         // check if player has clicked 'A' or 'D'
         // move left or right
+        // + gameScreen.GetComponent<RectTransform>().rect.width
+        // + gameScreen.GetComponent<RectTransform>().rect.width
+        // transform.position.x >= (gameCamera.transform.position.x - (gameCamera.fieldOfView/2))
 
-        if (Input.GetKey(KeyCode.A) && transform.position.x >= 0 )
+
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position -= new Vector3(velocity, 0, 0);
+            //transform.position -= new Vector3(Mathf.Clamp(transform.position.x, gameScreen.GetComponent<RectTransform>().rect.x, gameScreen.GetComponent<RectTransform>().rect.width), 0, 0);
         }
-        if (Input.GetKey(KeyCode.D) && transform.position.x <= screenWidth)
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(velocity, 0, 0);
         }
 
+    }
+
+    private void LateUpdate()
+    {
+        // clamping to screen
+        Vector3 viewPos = transform.position; // used to alter x and y
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x, screenBounds.x - 1);
+        transform.position = viewPos;
     }
 }
