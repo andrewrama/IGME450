6156283631aaac5a3453f344a230743aacd02de7
andrewrama <64484248+andrewrama@users.noreply.github.com>
2023-02-15 19:51:21 -0500

    started work on scrollview for cat inventory

--- Assets/Scripts/DisplayInventory.cs
+++ Assets/Scripts/DisplayInventory.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class DisplayInventory : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI catNameText;
+    public Transform contentContainer;
+    public GameObject inventoryItem;
 
     // Start is called before the first frame update
     void Start()
     {
-        string nameString = "";
 
         for (int i = 0; i < CatInventory.Instance.ownedCats.Count; i++)
         {
-            nameString += CatInventory.Instance.ownedCats[i].catName + "\n";
+            Cat currentCat = CatInventory.Instance.ownedCats[i];
+            var listItem = Instantiate(inventoryItem);
+
+            listItem.GetComponentsInChildren<Image>()[1].sprite = currentCat.imageSprite;
+            listItem.GetComponentsInChildren<TMPro.TextMeshProUGUI>()[0].text = currentCat.catName;
+            listItem.GetComponentsInChildren<TMPro.TextMeshProUGUI>()[1].text = currentCat.rarity;
+
+            listItem.transform.SetParent(contentContainer);
+            listItem.transform.localScale = Vector2.one;
         }
 
-        catNameText.text = nameString;
+        
     }
 
 }
