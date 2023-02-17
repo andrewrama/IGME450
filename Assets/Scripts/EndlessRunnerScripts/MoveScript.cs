7b0fb4bbbea6a054ac85f08e9d34361e74ba7f25
Jabrecia Washington <jlw4958@g.rit.edu>
2023-02-16 23:57:00 -0500

    naming standards T^T

--- Assets/Scripts/EndlessRunnerScripts/MoveScript.cs
+++ Assets/Scripts/EndlessRunnerScripts/MoveScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveScript : MonoBehaviour
+public class MoveScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
