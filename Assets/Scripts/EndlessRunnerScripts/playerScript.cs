1c3043ce16acb20ad705a39ff1e72239a43dae96
Jabrecia Washington <jlw4958@g.rit.edu>
2023-02-24 11:53:43 -0500

    updates to endless runner
    issue: collision detection not functional

--- Assets/Scripts/EndlessRunnerScripts/playerScript.cs
+++ Assets/Scripts/EndlessRunnerScripts/playerScript.cs
@@ -7,11 +7,13 @@ public class PlayerScript : MonoBehaviour
 {
     public int velocity;
     public Vector2 screenBounds;
+    public LogicScript logic;
 
     // Start is called before the first frame update
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); //gives us half the screen width and half the screen height (but they're negative values!)
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
@@ -38,5 +40,28 @@ public class PlayerScript : MonoBehaviour
         Vector3 viewPos = transform.position; // used to alter x and y
         viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x*2, screenBounds.x*6);
         transform.position = viewPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("COLLISION");
+        if(collision.gameObject.CompareTag("treat"))
+        {
+            Debug.Log("Collided with a treat!");
+        }
+        else if(collision.gameObject.CompareTag("box"))
+        {
+            Debug.Log("Collided with a box!");
+        }
+
+        //if (collision.gameObject.layer == 7)
+        //{
+        //    Debug.Log("Collided with a treat!");
+        //    logic.getTreat();
+        //}
+        //if (collision.gameObject.layer == 6)
+        //{
+        //    Debug.Log("Collided with a box!");
+        //}
     }
 }
