7b1869ffdbf3e7ee34a82a905de06b4b94b86535
Jabrecia Washington <jlw4958@g.rit.edu>
2023-03-10 12:17:17 -0500

    clean up in object spawner script

--- Assets/Scripts/EndlessRunnerScripts/ObjectSpawner.cs
+++ Assets/Scripts/EndlessRunnerScripts/ObjectSpawner.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    //public GameObject boxPrefab;
-    //public GameObject treatPrefab;
     public GameObject spawnedObject;
     private Vector2 screenBounds;
     public LogicScript logic;
@@ -14,19 +12,13 @@ public class ObjectSpawner : MonoBehaviour
 
     // timer variables
     public float spawnRate;
-    //public float boxSpawnRate = 1.5f;
-    //public float treatSpawnRate = 3f;
     public float timer = 0;
-    //public float boxTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        //posOptions = new float[3] { logic.centerPoint, logic.leftEdge, logic.rightEdge };
-        //Debug.Log(posOptions[0]);
-        //Debug.Log(posOptions[1]);
-        //Debug.Log(posOptions[2]);
+
     }
 
     // Update is called once per frame
@@ -36,17 +28,6 @@ public class ObjectSpawner : MonoBehaviour
         //  center
         //  leftEdge
         //  rightEdge
-
-        //if (boxTime < boxSpawnRate)
-        //{
-        //    boxTime += Time.deltaTime;
-        //}
-        //else
-        //{
-        //    Instantiate(boxPrefab, new Vector3(xPos, transform.position.y, 0), transform.rotation);
-        //    Debug.Log("Spawn a box!");
-        //    boxTime = 0;
-        //}
 
         if (timer < spawnRate)
         {
