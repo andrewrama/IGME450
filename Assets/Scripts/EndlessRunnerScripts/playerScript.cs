ac5dba99056ad00d4e59b113b1261ec0413733cb
Jabrecia Washington <jlw4958@g.rit.edu>
2023-03-09 23:34:05 -0500

    clean up
    moved PlayerController and LogicScript into EndlessRunnerScripts folder

--- Assets/Scripts/EndlessRunnerScripts/playerScript.cs
+++ Assets/Scripts/EndlessRunnerScripts/playerScript.cs
@@ -8,14 +8,7 @@ using UnityEngine.InputSystem;
 public class PlayerScript : MonoBehaviour
 {
     private PlayerControls playerControls;
-
-    private int velocity = 1;
-    private Vector2 screenBounds;
     public LogicScript logic;
-    private float currentX;
-    //public InputAction leftAction;
-    //public InputAction rightAction;
-
 
     // center is 108
     // furthest left side is 54 
@@ -24,8 +17,6 @@ public class PlayerScript : MonoBehaviour
     private void Awake()
     {
         playerControls = new PlayerControls();
-        //leftAction.performed += MoveLeft;
-        //rightAction.performed += MoveRight;
     }
 
     private void OnEnable()
@@ -41,81 +32,25 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); //gives us half the screen width and half the screen height (but they're negative values!)
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-
-        //playerControls.PlayerMovement.MoveLeft.performed += MoveLeft;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentX = transform.position.x;
-
         if (playerControls.PlayerMovement.MoveLeft.triggered)
         {
             MoveLeft();
-            Debug.Log("Left!");
         }
         
         if (playerControls.PlayerMovement.MoveRight.triggered)
         {
             MoveRight();
-            Debug.Log("Right!");
         }
-
-        // check if player has clicked 'A' or 'D'
-        // move left or right
-
-        //// left
-        //if (Input.GetKeyDown(KeyCode.A)) // || gesture
-        //{
-        //    Debug.Log("A pressed");
-        //    if (transform.position.x == logic.centerPoint)
-        //    {
-        //        transform.position = new Vector3(logic.leftEdge, 194, 0);
-        //    }
-        //    else if (transform.position.x > logic.centerPoint)
-        //    //else if(transform.position.x == logic.rightEdge)
-        //    {
-        //        transform.position = new Vector3(logic.centerPoint, 194, 0);
-        //    }
-        //
-        //}
-
-        //// right
-        //if (Input.GetKeyDown(KeyCode.D))
-        //{
-        //    Debug.Log("D pressed");
-        //    if (transform.position.x == logic.centerPoint)
-        //    {
-        //        transform.position = new Vector3(logic.rightEdge, 194, 0);
-        //    }
-        //    else if (transform.position.x < logic.centerPoint)
-        //    //else if (transform.position.x == logic.leftEdge)
-        //    {
-        //        transform.position = new Vector3(logic.centerPoint, 194, 0);
-        //    }
-        //
-        //
-        //}
-
     }
 
-    //public void MoveLeft(InputAction.CallbackContext context)
     public void MoveLeft()
-
     {
-        //    if (transform.position.x == logic.centerPoint)
-        //    {
-        //        transform.position = new Vector3(logic.leftEdge, 194, 0);
-        //    }
-        //    else if (transform.position.x > logic.centerPoint)
-        //    //else if(transform.position.x == logic.rightEdge)
-        //    {
-        //        transform.position = new Vector3(logic.centerPoint, 194, 0);
-        //    }
-
         if (transform.position.x != logic.leftEdge)
         {
             if (transform.position.x == logic.centerPoint)
@@ -129,19 +64,8 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    //public void MoveRight(InputAction.CallbackContext context)
     public void MoveRight()
     {
-        //if (transform.position.x == logic.centerPoint)
-        //{
-        //    transform.position = new Vector3(logic.rightEdge, 194, 0);
-        //}
-        //else if (transform.position.x < logic.centerPoint)
-        ////else if (transform.position.x == logic.leftEdge)
-        //{
-        //    transform.position = new Vector3(logic.centerPoint, 194, 0);
-        //}
-
         if (transform.position.x != logic.rightEdge)
         {
             if (transform.position.x == logic.centerPoint)
