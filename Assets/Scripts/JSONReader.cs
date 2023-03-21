8dde8214a7da1f41e668714c847666093e9548dd
Nya <nyabentley@gmail.com>
2023-03-21 13:49:07 -0400

    renamed variables

--- Assets/Scripts/JSONReader.cs
+++ Assets/Scripts/JSONReader.cs
@@ -7,13 +7,13 @@ public class JSONReader : MonoBehaviour
     public TextAsset textJSON;
 
     //all of the cats in the game
-    public List<global::Cat> catPool;
+    public List<Cat> catPool;
 
     //all of the cats the player owns
-    public List<global::Cat> ownedCats;
+    public List<Cat> ownedCats;
 
     [System.Serializable]
-    public class Cat
+    public class JsonCat
     {
         public string name;
         public string imgPath;
@@ -21,13 +21,19 @@ public class JSONReader : MonoBehaviour
     }
 
     [System.Serializable]
-    public class JsonCatList
+    public class JsonData
     {
-        public List<Cat> allCats;
+        public List<JsonCat> allCats;
+        public List<JsonCat> ownedCats;
 
-        public void PrintCatCount()
+        public void PrintAllCatsCount()
         {
             Debug.Log(allCats.Count);
+        }
+
+        public void PrintOwnedCatsCount()
+        {
+            Debug.Log(ownedCats.Count);
         }
     }
 
@@ -35,26 +41,52 @@ public class JSONReader : MonoBehaviour
     void Start()
     {
         Debug.Log(textJSON.text);
-        JsonCatList jsonCats = JsonUtility.FromJson<JsonCatList>(textJSON.text);
+        JsonData jsonCats = JsonUtility.FromJson<JsonData>(textJSON.text);
 
-        jsonCats.PrintCatCount();
+        catPool = new List<Cat>();
+        ownedCats = new List<Cat>();
 
-        List<global::Cat> catCollection = new List<global::Cat>();
+        jsonCats.PrintAllCatsCount();
+        jsonCats.PrintOwnedCatsCount();
 
+        //get all the possible cats
         for (int i = 0; i < jsonCats.allCats.Count; i++)
         {
-            Sprite image = LoadImage(i, jsonCats);
-            Cat c = jsonCats.allCats[i];
+            Sprite image = LoadAllCatImage(i, jsonCats);
+            JsonCat a = jsonCats.allCats[i];
 
-            global::Cat cat = new global::Cat(image, c.name, c.rarity);
-            cat.Print();
-            catCollection.Add(cat);
+            Cat allCat = new Cat(image, a.name, a.rarity);
+            catPool.Add(allCat);
+
+        }
+
+        //get a list of all the owned cats
+        for (int i = 0; i < jsonCats.ownedCats.Count; i++)
+        {
+            JsonCat o = jsonCats.ownedCats[i];
+            Sprite image = LoaOwnedCatImage(i, jsonCats);
+
+            Cat ownedCat = new Cat(image, o.name, o.rarity);
+            ownedCats.Add(ownedCat);
         }
     }
 
-    private Sprite LoadImage(int index, JsonCatList catPool)
+    /// <summary>
+    /// Helper method in order to get the images for the cats
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="catPool"></param>
+    /// <returns></returns>
+    private Sprite LoadAllCatImage(int index, JsonData catPool)
     {
         string imageURL = Application.dataPath + catPool.allCats[index].imgPath;
+
+        return (Sprite)AssetDatabase.LoadAssetAtPath(imageURL, typeof(Sprite));
+    }
+
+    private Sprite LoaOwnedCatImage(int index, JsonData catPool)
+    {
+        string imageURL = Application.dataPath + catPool.ownedCats[index].imgPath;
 
         return (Sprite)AssetDatabase.LoadAssetAtPath(imageURL, typeof(Sprite));
     }
